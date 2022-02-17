fc77254428cc47237055533c1deffd4891d50bf1
Anon Ray <rayanon004@gmail.com>
2022-02-17 16:23:04 +0530

    improve the UI a bit

--- Makefile
+++ Makefile
@@ -3,10 +3,14 @@ dev: build
 	cd dist/ && python -m http.server
 
 build: src
-	mkdir -p dist/
+	mkdir -p dist/data/
 	cp src/index.html dist/
 	cp src/index.js dist/
 	cp src/styles.css dist/
-	cp -r data dist/
+	cp -r data/*.geojson dist/data/
+	cp -r data/*.geojson.gz dist/data/
 
-.PHONY: dev
+clean:
+	rm -rf dist/
+
+.PHONY: dev clean
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+(function() {
+
 // Constants
 OSM_ATTRIBUTION = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
 TOKEN = "pk.eyJ1IjoiZWN0aGllbmRlciIsImEiOiJja2xxcmNsbncxNWs3MnBxbTB2cWxzNzJwIn0.IhuSiCpVvP4K2JQQtXezuw"
 OSM_DATA_URL = "/data/india-rivers.geojson"
 
-RIVER_COLOR="#2976F2"
-RIVER_COLOR_COMPL1="#A66F00"
-RIVER_COLOR_COMPL2="#F2B029"
+// the color of the water in the leaflet map
+LEAFLET_WATER_COLOR="#75cff0"
+RIVER_COLOR="#6ABBD9"
+RIVER_COLOR_ALT="#E67929"
+
 /* Original list of all languages the data set has
 __LANGS__ = ["af","al","am","an","ar","as","az","be","bg","bn","bo","br","bs","ca","cs","cy","da","de","dv","el","en","eo","es","et","eu","fa","fi","fr","ga","gl","gu","he","hi","hr","hu","hy","ia","id","is","it","ja","jp","jv","ka","kk","kn","ko","ku","la","lb","li","lt","lv","mk","ml","mr","ms","my","ne","nl","nn","no","oc","or","pa","pl","ps","pt","ro","ru","sa","sd","sk","sl","sr","sv","sw","ta","te","tg","th","tk","tl","tr","uk","ur","vi","yi","zh"];
 */
@@ -13,7 +17,16 @@ __LANGS__ = ["af","al","am","an","ar","as","az","be","bg","bn","bo","br","bs","c
 LANGS = ["alt","ar","as","bn","en","gu","hi","kn","ml","mr","ne","or","pa","sa","sd","ta","te","ur"].map((l) => "name:" + l);
 
 function runViz() {
-  var mapL = L.map("map").setView([20.67085, 78.87206], 5);
+
+  const screenWidth = window.screen.width
+  const screenHeight = window.screen.height
+
+  const riverLineWeight = screenWidth > 1920 ? 4 : 3
+  const initCoords = [20.67085, 78.87206]
+  // FIXME: set this initial coordinates based on screen size. The below looks good for 1366x768 screens
+  const initZoomLevel = screenWidth > 1920 ? 6 : 5
+
+  const mapL = L.map('map').setView(initCoords, initZoomLevel);
 
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={token}", {
     attribution: OSM_ATTRIBUTION,
@@ -24,6 +37,7 @@ function runViz() {
     token: TOKEN
   }).addTo(mapL);
 
+  // fetch the rivers data and initialize it on the map
   fetch(OSM_DATA_URL)
   .then(res => res.json())
   .then(data => {
@@ -32,58 +46,68 @@ function runViz() {
   })
   .catch(error => {
     console.log('error fetching data', error);
-    alert('Some error occurred while fetching data from server. Please try again in some time.')
+    alert('Some error occurred while fetching data from the server. Please try again in some time.')
   });
 }
 
 function addRivers(mapL, data) {
   const riverStyles = {
     color: RIVER_COLOR,
-    weight: 3,
+    weight: 4,
     opacity: 1
-  };
+  }
 
   L.geoJSON(data, {
     style: riverStyles,
-    onEachFeature: featureLogic
-  }).addTo(mapL);
+    onEachFeature: riverDetailsPopup
+  }).addTo(mapL)
 }
 
-function featureLogic(feature, layer) {
+function riverDetailsPopup(feature, layer) {
   // set on hover styles
-  layer.on('mouseover', () => layer.setStyle({color: RIVER_COLOR_COMPL2}));
+  layer.on('mouseover', () => layer.setStyle({color: RIVER_COLOR_ALT}));
   layer.on('mouseout', () => layer.setStyle({color: RIVER_COLOR}));
 
   // set on hover tooltip
-  const name = getName(feature.properties);
-  const title = "<div>" + name + "</div>";
-  layer.bindTooltip(title, {sticky: true, className: "river-tooltip"});
+  const title = `<div>${getRiverName(feature.properties)}</div>`
+  layer.bindTooltip(title, {sticky: true, className: 'river-tooltip'});
 
   // set onclick popup
-  const details = makeDetails(feature.properties);
-  layer.bindPopup(details);
+  layer.bindPopup(makeDetails(feature.properties));
 }
 
 function makeDetails(props) {
-  let html = "<h3>" + getName(props) + "</h3>";
+  let html = `<div class="popup-title"> ${getRiverName(props)} </div>`;
   const availableLangs = LANGS.filter((lang) => props.hasOwnProperty(lang));
   const otherNames = availableLangs.map((lang) => props[lang]);
-  html += "<p><b>Other names</b>: " + sanitizeDisplay(otherNames.join(", ")) + "</p>";
-  html += "<p><b>Tributary of</b>: " + sanitizeDisplay(props["tributary_of"]) + "</p>";
-  html += makeWikiLink(props["wikipedia"]);
-  return html;
+  html += makeDetail('Source', props["source"] || props['source:name:uk'])
+  html += makeDetail('Destination', props["destination"])
+  html += makeDetail('Tributary of', props["tributary_of"])
+  html += makeDetail('Role', props["role"])
+  html += makeDetail('Service', props["service"])
+  html += makeDetail('Width', props["width"])
+  html += makeDetail('Other names', otherNames.join(', '))
+  html += makeWikiLink(props["wikipedia"])
+  return html
+}
+
+function makeDetail(label, value) {
+  if (!value) {
+    return ''
+  }
+  return `<div><b>${label}</b>: ${value} </div>`
 }
 
 function makeWikiLink(wiki) {
   if (wiki) {
-    const href = "https://wikipedia.org/wiki/" + encodeURIComponent(wiki);
-    return "<p> More details: <a target='_blank' href=\"" + href + "\">Wikipedia</a> </p>";
+    const href = `https://wikipedia.org/wiki/${encodeURIComponent(wiki)}`
+    return `<p> <a target='_blank' href="${href}">Learn More</a> </p>`
   }
-  return "";
+  return ''
 }
 
 // try to name of the river from various possible attributes
-function getName(props) {
+function getRiverName(props) {
   const validAttrs = ["name", "name:en", "alt_name", "name:alt"];
   for (var i = 0; i < validAttrs.length; i++) {
     let p = validAttrs[i];
@@ -94,11 +118,6 @@ function getName(props) {
   return "<i>Unknown</i>";
 }
 
-function sanitizeDisplay(str) {
-  if(!str) {
-    return "<i> Data not available </i>";
-  }
-  return str;
-}
-
 window.onload = runViz;
+
+})()
